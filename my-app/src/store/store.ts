7716b168b765467e29afafd1f333b1b8bc7e0852
unknown <timur.dev@mail.ru>
2022-05-10 02:51:59 +0300

    feat: add columnsApi to store

--- my-app/src/store/store.ts
+++ my-app/src/store/store.ts
@@ -1,15 +1,18 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { boardsApi } from '../services/boardsService';
+import boardsApi from '../services/boardsService';
+import columnsApi from '../services/columnsService';
 import userReducer from './reducers/userSlice';
 
 const rootReducer = combineReducers({
   userReducer,
   [boardsApi.reducerPath]: boardsApi.reducer,
+  [columnsApi.reducerPath]: columnsApi.reducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(boardsApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(boardsApi.middleware).concat(columnsApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
