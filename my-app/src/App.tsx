b763c9f466ad1e9c5f57f7e591b1c45745189e1f
unknown <timur.dev@mail.ru>
2022-05-09 16:17:31 +0300

    refactor: move Footer & Authors to a folders, move styles in separate files

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,7 +6,7 @@ import { Route, Routes } from 'react-router-dom';
 import i18n from './localization/i18n';
 import LanguageToggle from './components/LanguageToggle/LanguageToggle';
 import { useTranslation } from 'react-i18next';
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header';
 import Login from './components/Login';
 import Main from './components/Main';
