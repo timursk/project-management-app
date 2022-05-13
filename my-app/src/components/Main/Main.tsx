1343bf1531f5bfa96f6789e795dd914c9f06e136
timursk <timur.dev@mail.ru>
2022-05-13 19:13:23 +0300

    feat: add main`s header: info, input & profile btn

--- my-app/src/components/Main/Main.tsx
+++ my-app/src/components/Main/Main.tsx
@@ -1,4 +1,13 @@
-import { Grid, IconButton, Tooltip, Typography, Zoom } from '@mui/material';
+import {
+  Grid,
+  IconButton,
+  InputBase,
+  TextField,
+  Toolbar,
+  Tooltip,
+  Typography,
+  Zoom,
+} from '@mui/material';
 import React, { FC, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
@@ -6,9 +15,33 @@ import boardsApi from '../../services/boardsService';
 import BoardCard from '../BoardCard/BoardCard';
 import Loader from '../Loader/Loader';
 import { StyledGridItem, StyledGrid, StyledAddCircleIcon } from './styles';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import styled from '@emotion/styled';
+import SearchIcon from '@mui/icons-material/Search';
 
 const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxOTNiNjY4NS00OTA0LTRlNGMtYWM5MS00MGRjNjBhN2JlZTkiLCJsb2dpbiI6InRlc3QiLCJpYXQiOjE2NTIyNjg3NzF9.z3z283PgbUDkcblzNR-SZO01qW68dRPGWQxLy-X_ydQ';
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0px 30px 60px rgba(32, 56, 85, 0.15);
+  border-radius: 8px;
+  padding: 12px 32px 12px 24px;
+  height: 50px;
+  margin: 15px 0;
+`;
+
+{
+  /* <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+  <Typography variant="h2">{`${t('main.header')}`}</Typography>
+
+  <IconButton size="large" onClick={() => {}}>
+    <AccountCircle sx={{ width: 40, height: 40 }} />
+  </IconButton>
+</div>; */
+}
 
 const Main: FC = () => {
   const { t } = useTranslation();
@@ -35,7 +68,47 @@ const Main: FC = () => {
 
   return (
     <>
-      <Typography variant="h2">{`${t('main.header')} (${data?.length || 0})`}</Typography>
+      <StyledDiv>
+        <div style={{ display: 'flex', flex: '7 1' }}>
+          <Typography sx={{ mr: 5 }} variant="h6">{`${t('main.header')}`}</Typography>
+          {/* <TextField id="standard-basic" label="Standard" variant="standard" /> */}
+          <div style={{ position: 'relative', width: '100%' }}>
+            <InputBase
+              sx={{
+                ml: 1,
+                flex: 1,
+                background: '#F4F7FC',
+                borderRadius: '200px',
+                width: '100%',
+                paddingLeft: '45px',
+                paddingRight: '15px',
+              }}
+              placeholder="Search"
+              inputProps={{ 'aria-label': 'search google maps' }}
+            />
+
+            <IconButton
+              type="submit"
+              sx={{
+                p: '10px',
+                position: 'absolute',
+                top: '50%',
+                left: '10px',
+                transform: 'translateY(-50%)',
+              }}
+              aria-label="search"
+            >
+              <SearchIcon />
+            </IconButton>
+          </div>
+        </div>
+        <div style={{ flex: '3 1', display: 'flex', flexDirection: 'row-reverse' }}>
+          <IconButton size="large" onClick={() => {}}>
+            <AccountCircle sx={{ width: 40, height: 40 }} />
+          </IconButton>
+        </div>
+      </StyledDiv>
+
       <StyledGrid container spacing={4}>
         {data &&
           data.map(({ id, title }) => {
