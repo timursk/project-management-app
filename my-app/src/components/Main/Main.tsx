0ce47b4b70775d8e90d0523a82ad94437ba7839e
unknown <timur.dev@mail.ru>
2022-05-14 03:26:21 +0300

    refactor: remove ling warnings, delete commented code

--- my-app/src/components/Main/Main.tsx
+++ my-app/src/components/Main/Main.tsx
@@ -1,38 +1,15 @@
-import {
-  Grid,
-  IconButton,
-  InputBase,
-  TextField,
-  Toolbar,
-  Tooltip,
-  Typography,
-  Zoom,
-} from '@mui/material';
-import React, { ChangeEvent, FC, useCallback, useEffect, useState } from 'react';
+import { Grid, IconButton, Tooltip, Zoom } from '@mui/material';
+import React, { FC, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import boardsApi from '../../services/boardsService';
 import BoardCard from '../BoardCard/BoardCard';
 import Loader from '../Loader/Loader';
 import { StyledGridItem, StyledGrid, StyledAddCircleIcon } from './styles';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import styled from '@emotion/styled';
-import SearchIcon from '@mui/icons-material/Search';
-import ClearIcon from '@mui/icons-material/Clear';
 import MainControls from '../MainControls/MainControls';
 
 const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxOTNiNjY4NS00OTA0LTRlNGMtYWM5MS00MGRjNjBhN2JlZTkiLCJsb2dpbiI6InRlc3QiLCJpYXQiOjE2NTIyNjg3NzF9.z3z283PgbUDkcblzNR-SZO01qW68dRPGWQxLy-X_ydQ';
-
-{
-  /* <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-  <Typography variant="h2">{`${t('main.header')}`}</Typography>
-
-  <IconButton size="large" onClick={() => {}}>
-    <AccountCircle sx={{ width: 40, height: 40 }} />
-  </IconButton>
-</div>; */
-}
 
 const Main: FC = () => {
   const { t } = useTranslation();
@@ -89,7 +66,7 @@ const Main: FC = () => {
         <StyledGridItem item xs={12} sm={6} md={4}>
           <Tooltip title={t('main.add')} placement="left" TransitionComponent={Zoom}>
             <IconButton onClick={handleAdd}>
-              <StyledAddCircleIcon sx={{ width: 80, height: 80 }} />
+              <StyledAddCircleIcon />
             </IconButton>
           </Tooltip>
         </StyledGridItem>
