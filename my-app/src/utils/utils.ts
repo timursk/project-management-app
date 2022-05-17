023326b47bda4118d59e11590000ee6d2f58290e
timursk <timur.dev@mail.ru>
2022-05-17 16:57:35 +0300

    feat: add get, set, del token functions to utils, add it to thunk. Rebase develop

--- my-app/src/utils/utils.ts
+++ my-app/src/utils/utils.ts
@@ -7,3 +7,16 @@ export const getTaskUrl = (boardId: string, columnId: string, taskId?: string) =
   `${ENDPOINTS.BOARDS}/${boardId}${ENDPOINTS.COLUMNS}/${columnId}${ENDPOINTS.TASKS}${
     taskId ? `/${taskId}` : ''
   }`;
+
+export const getToken = () => {
+  const token = window.localStorage.getItem('token');
+  return token;
+};
+
+export const setToken = (token: string) => {
+  window.localStorage.setItem('token', token);
+};
+
+export const removeToken = () => {
+  window.localStorage.removeItem('token');
+};
