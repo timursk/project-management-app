0678c875871f8719c1ff1a554ac8b4987b9f3e0e
unknown <timur.dev@mail.ru>
2022-05-23 03:39:24 +0300

    feat: add users interface in MainControls

--- my-app/src/components/MainControls/MainControls.tsx
+++ my-app/src/components/MainControls/MainControls.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import React, { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
 import { t } from 'i18next';
 import {
   StyledDivContainer,
@@ -10,13 +10,39 @@ import {
 } from './styles';
 
 import ClearIcon from '@mui/icons-material/Clear';
+import { IconButton, Tooltip, Zoom, Avatar, Chip, Badge, Box } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import styled from '@emotion/styled';
 
 type Props = {
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
 };
 
+const StyledBox = styled(Box)`
+  position: absolute;
+  top: 100%;
+  right: 0;
+  width: 200px;
+  z-index: 100;
+  flex-wrap: wrap;
+  background: lightgrey;
+  gap: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 150px;
+  overflow: auto;
+  padding: 5px;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
 const MainControls = ({ value, setValue }: Props) => {
+  const [isListShown, setIsListShown] = useState<boolean>(false);
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setValue(value);
@@ -24,6 +50,10 @@ const MainControls = ({ value, setValue }: Props) => {
 
   const handleDelete = () => {
     setValue('');
+  };
+
+  const handleClick = () => {
+    setIsListShown(!isListShown);
   };
 
   return (
@@ -41,6 +71,22 @@ const MainControls = ({ value, setValue }: Props) => {
           </StyledIconButton>
         )}
       </StyledDivRelative>
+
+      <Wrapper>
+        <Tooltip title="Users" TransitionComponent={Zoom}>
+          <IconButton onClick={handleClick}>
+            <Badge badgeContent={4} color="primary">
+              <AccountCircleIcon />
+            </Badge>
+          </IconButton>
+        </Tooltip>
+
+        {isListShown && (
+          <StyledBox>
+            <Chip avatar={<Avatar>M</Avatar>} label="Avatar" />
+          </StyledBox>
+        )}
+      </Wrapper>
     </StyledDivContainer>
   );
 };
