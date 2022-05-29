b3d5666c79c35018bd6f0e4fff6cf46b7be2a230
komalapa <komalapa@gmail.com>
2022-05-30 00:09:17 +0400

    fix: https back for netlify

--- my-app/src/utils/constants.ts
+++ my-app/src/utils/constants.ts
@@ -1,4 +1,5 @@
-export const API_URL = 'http://82.146.49.187:4000';
+// export const API_URL = 'http://82.146.49.187:4000';
+export const API_URL = 'https://komalapa.ru';
 
 export const ENDPOINTS = {
   USERS: '/users',
